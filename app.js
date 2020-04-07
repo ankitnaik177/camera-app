b36be08678bb95ae4137a89242a6f1ac6c1c716a
ankitnaik177 <39136090+ankitnaik177@users.noreply.github.com>
2020-04-07 23:09:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
@@ -16,6 +17,13 @@ function cameraStart() {
         console.error("Oops. Something is broken.", error);
     });
 }
-
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+};
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
